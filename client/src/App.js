06179730b75c4d8ad113a5b57229b2a7e573aa34
vladbronfman <vlad.bronfman@gmail.com>
2020-08-26 14:16:49 +0300

    testing deployment

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
 
-
 class App extends Component {
   render() {
     return (
